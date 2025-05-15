7a70de84ece24bcea2fce41cf4cf69ae5a0eccc3
a1916700 <zihan.luo@student.adelaide.edu.au>
2025-05-15 09:45:49 +0930

    all

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config(); // 加载 .env 文件
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
