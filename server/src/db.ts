c5c7eb01750b0d6a310e6d26b01e0430f371b6d8
tula163 <simon_tula@163.com>
2025-09-02 21:40:35 +0930

    update

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = "mongodb://localhost:27017/interview_hub"; 
+const MONGO_URI = "mongodb://localhost:27017/interview_hub";
 
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
     console.log("✅ Connected to MongoDB");
   } catch (error) {
-
-    process.exit(1);
+    console.error("❌ MongoDB connection error:", error);
+    // process.exit(1);
   }
 };
